be9b6a0af0743e04c10ce701516f4bc0b4fbd1e0
dmiv <dmitriy40647274@gmail.com>
2023-10-28 10:54:42 +0300

    корректный вывод изоражения

--- src/views/ExampleView.vue
+++ src/views/ExampleView.vue
@@ -8,12 +8,15 @@ import { ref, onMounted} from "vue";
 
 const canvas = ref(null)
 const ctx = ref(null)
-
+const width = ref(window.innerWidth * 0.6)
+const height =ref( window.innerHeight)
+// console.log(width)
+// console.log(width.value)
 onMounted(() => {
   canvas.value = document.getElementById('canvas')
   ctx.value = canvas.value.getContext('2d')
-  canvas.value.width = window.innerWidth * 0.6
-  canvas.value.height = window.innerHeight * 0.6
+  canvas.value.width = width.value
+  canvas.value.height = height.value
   let background = new Image()
   // background.src = '/img/img.709694e9.png'
   let pathToImage = document.getElementById('image').src
@@ -21,9 +24,9 @@ onMounted(() => {
   background.src = pathToImage
 
   background.onload = function () {
-    ctx.value.drawImage(background,0,0)
+    ctx.value.drawImage(background,0,0, width.value, height.value)
   }
-  ctx.value.fillStyle = "rgba(0, 0, 200, 0.5)"
+  // ctx.value.fillStyle = "rgba(0, 0, 200, 0.5)"
 
   // ctx.value.fillRect(30, 30, 55, 50)
 
