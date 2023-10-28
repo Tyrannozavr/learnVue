84b528e48f8e2c76f10341c8403eb3bee981e981
dmiv <dmitriy40647274@gmail.com>
2023-10-28 11:44:00 +0300

    div-ы рисуются поверх canvas

--- src/views/ExampleView.vue
+++ src/views/ExampleView.vue
@@ -1,42 +1,42 @@
 <template>
-  <canvas id="canvas"></canvas>
+
+  <div id="zoom-container" style="position: relative">
+    <canvas id="defaultCanvas0" class="" style="width: 753px; height: 950px;" width="1506" height="1900"></canvas>
+    <div style="position: absolute; height: 100px; width: 400px; top: 300px; left: 250px">
+      <a href="#hello">1</a>
+    </div>
+  </div>
+
   <img id="image" src="@/assets/img/details.jpg" style="display: none">
 </template>
 
 <script setup>
-import { ref, onMounted} from "vue";
+import {onMounted, ref} from "vue";
 
 const canvas = ref(null)
 const ctx = ref(null)
 const width = ref(window.innerWidth * 0.6)
 const height =ref( window.innerHeight)
-// console.log(width)
-// console.log(width.value)
 onMounted(() => {
-  canvas.value = document.getElementById('canvas')
+  canvas.value = document.getElementById('defaultCanvas0')
   ctx.value = canvas.value.getContext('2d')
   canvas.value.width = width.value
   canvas.value.height = height.value
   let background = new Image()
-  // background.src = '/img/img.709694e9.png'
-  let pathToImage = document.getElementById('image').src
-  // console.log(pathToImage)
-  background.src = pathToImage
+  background.src = document.getElementById('image').src
 
   background.onload = function () {
+    ctx.value.background = 'rgb(0, 0, 255'
     ctx.value.drawImage(background,0,0, width.value, height.value)
+    // ctx.value.fillStyle = 'rgb(0,0,255)'
+    // ctx.value.fillRect(0, 0, 55, 55)
   }
-  // ctx.value.fillStyle = "rgba(0, 0, 200, 0.5)"
-
-  // ctx.value.fillRect(30, 30, 55, 50)
 
 })
 </script>
-
+<!--$(zoomPoints[i]).css('left',deltaX+item.x*allScale).css('top',deltaY+item.y*allScale).css('transform','scale('+allScale+')')-->
 <style scoped>
 canvas {
   border: 1px solid black;
 }
 </style>
-<!-- var pathToImg = document.getElementById('zoom-container').dataset.img;-->
-<!--<div class="zoom-container"  oncontextmenu="return false;" style="position: relative" id="zoom-container" data-img="/uploads/images/9806824bc8/190/Поршень коленвал Chery Tiggo 5_original.jpg"  data-width="2120" data-height="2073">-->
